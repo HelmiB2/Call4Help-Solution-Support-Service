afafd379edfddd8f4be89471d17d2c96be0f3274
HelmiB2 <bani.hel@gmail.com>
2025-10-17 10:29:27 +0200

    startviewSupporter

--- supporter/src/main/java/com/c4h/supporter/SupporterApp.java
+++ supporter/src/main/java/com/c4h/supporter/SupporterApp.java
@@ -1,7 +1,5 @@
 package com.c4h.supporter;
 
-import com.c4h.solutionsupport.startView.StartViewControler;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -16,24 +14,8 @@ public class SupporterApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         // StartView FXML laden (Desktop-Variante)
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/startDesktop.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/startviewSupporter.fxml"));
         Parent root = loader.load();
-
-        // Controller abgreifen
-        StartViewControler controller = loader.getController();
-
-        // Nur die Module aktivieren, die sichtbar sein sollen
-        controller.setEnabledModules("chat");   // Chat sichtbar
-        controller.setEnabledModules("Ticket"); // Ticket sichtbar
-        controller.setEnabledModules("intern"); // Mitarbeiter sichtbar
-        
-     // Dynamisches Verschieben des Buttons "Intern"
-        controller.getBtn3sMitarbeiter().visibleProperty().addListener((obs, oldVal, newVal) -> {
-            adjustInternButtonPosition(controller);
-        });
-
-        // Initial position anpassen
-        adjustInternButtonPosition(controller);
 
         // Bildschirmgröße ermitteln
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
@@ -73,24 +55,6 @@ public class SupporterApp extends Application {
         stage.setTitle("Supporter App");
         stage.show();
     }
-    private void adjustInternButtonPosition(StartViewControler controller) {
-        // Prüfen ob andere Buttons sichtbar sind
-        boolean otherVisible = controller.getBtnGeraeteInfo().isVisible();
-        
-        if (otherVisible) {
-            // Button bleibt in der HBox / normaler Platz
-            controller.getBtn3sMitarbeiter().setTranslateY(0);
-        } else {
-            // Button nach unten verschieben
-            double parentHeight = ((javafx.scene.layout.Region) controller.getBtn3sMitarbeiter().getParent()).getHeight();
-            double buttonHeight = controller.getBtn3sMitarbeiter().getLayoutBounds().getWidth();
-
-            
-            // Abstand zum unteren Rand z.B. 20px
-            controller.getBtn3sMitarbeiter().setTranslateY(parentHeight - buttonHeight - 20);
-        }
-    }
-
 
     public static void main(String[] args) {
         System.out.println("Supporter App startet...");
